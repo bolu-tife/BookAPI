55690c60d24050f9d691851c7af079273b79154b
Boluwatife Omosowon <boluomosowon@gmail.com>
2020-06-27 14:45:06 +0100

    rearranged

---
+++ BookAPI/Controllers/GenreController.cs
@@ -0,0 +1,87 @@
+﻿using System.Threading.Tasks;
+using BookAPI.Entities;
+using BookAPI.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class GenreController : ControllerBase
+    {
+        private IGenre _genre;
+        public GenreController(IGenre genre)
+        {
+            _genre = genre;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Genre genre)
+        {
+            _genre.Add(genre);
+        }
+
+        [HttpPost("AddGenre")]
+        public async Task<IActionResult> AddGenre([FromBody] Genre genre)
+        {
+            var createGenre = await _genre.AddAsync(genre);
+
+            if (createGenre)
+            {
+                return Ok("Genre Created");
+            }
+            else
+            {
+                return BadRequest(new { message = "Unable to create Genre details" });
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var genres = await _genre.GetAll();
+            return Ok(genres);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var genre = await _genre.GetById(id);
+            return Ok(genre);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Genre genre)
+        {
+            genre.Id = id;
+            var updateGenre = await _genre.Update(genre);
+
+            if (updateGenre)
+            {
+                return Ok("Genre Updated");
+            }
+            else
+            {
+                return BadRequest(new { message = "Unable to update Genre details" });
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleteGenre = await _genre.Delete(id);
+            if (deleteGenre)
+            {
+                return Ok("Genre Deleted");
+            }
+            else
+            {
+                return BadRequest(new { message = "Unable to delete Genre details" });
+            }
+        }
+    }
+}
---
+++ BookAPI/Controllers/RoleController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookAPI.Dto;
+using BookAPI.Entities;
+using BookAPI.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    //[Authorize]
+    public class RoleController : ControllerBase
+    {
+        private IRole _role;
+        public RoleController(IRole role)
+        {
+            _role = role;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] RoleModels registerRole)
+        {
+            ApplicationRole role = new ApplicationRole();
+
+            role.Name = registerRole.Name;
+            
+
+
+            var newRole = await _role.CreateRole(role);
+            if (newRole)
+                return Ok(new { message = "Role Created" });
+
+            return BadRequest(new { message = "Role not created" });
+        }
+
+        //[HttpPost("authenticate")]
+        //public async Task<IActionResult> Authenticate([FromBody] LoginDto userDto)
+        //{
+        //    var user = await _account.SignIn(userDto);
+
+        //    if (user == null)
+        //        return BadRequest(new { message = "Username or password is incorrect" });
+
+        //    return Ok(user);
+        //}
+
+        //[AllowAnonymous]
+        //GetAll
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var roles = await _role.GetAll();
+            return Ok(roles);
+        }
+        //GetByUserId
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByRoleId(string id)
+        {
+            var role = await _role.GetByRoleId(id);
+            return Ok(role);
+        }
+
+        //Update
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] ApplicationRole approle)
+        {
+            approle.Id = id;
+            var updaterole = await _role.UpdateUser(approle);
+
+            if (updaterole)
+            {
+                return Ok("Role Updated");
+            }
+            else
+            {
+                return BadRequest(new { message = "Unable to update Role details" });
+            }
+        }
+
+        //// DELETE 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var deleteRole = await _role.Delete(id);
+            if (deleteRole)
+            {
+                return Ok("Role Deleted");
+            }
+            else
+            {
+                return BadRequest(new { message = "Unable to delete ROle details" });
+            }
+        }
+    }
+}
